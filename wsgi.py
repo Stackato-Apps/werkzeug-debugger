fe1fce6be707a936ca101d1bac4ffbd1f10538d4
Sridhar Ratnakumar <sridharr@activestate.com>
2012-04-20 10:54:14 -0700

    use PORT and requirements.txt

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,4 @@
+import os
 from werkzeug.wrappers import Request, Response
 from werkzeug.debug import DebuggedApplication
 
@@ -11,4 +12,5 @@ application = DebuggedApplication(application, evalex=True)
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
-    run_simple('localhost', 4000, application)
+    port = int(os.environ.get("PORT", 5000))
+    run_simple('0.0.0.0', port, application)
